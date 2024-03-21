e48e2f8f51ab3bb3dedae0911d1fe78851d46ebb
FedeC21 <94315894+FedeC21@users.noreply.github.com>
2024-03-21 15:14:29 +0100

    created s3 bucket connector

---
+++ xetra/common/s3.py
@@ -0,0 +1,28 @@
+'''Connector adn methods accessing S3'''
+import os 
+import boto3
+
+class S3BucketConnector():
+    '''Class for interacting with S3bucket'''
+    def __init__(self, access_key: str , secret_key:str, endpoint_url:str,bucket:str ):
+        '''Constructor for S3BucketConnector
+        
+        :param access_key: acces key for accessing S3   
+        :param secret_key: secret key for accessing S3
+        :param endpoint_url: enpoint to url S3
+        :param bucket: S3 bucket name 
+        '''
+        self.enpoint_url = endpoint_url
+        self.session = boto3.Session(aws_access_key_id = os.environ[access_key],
+                                     aws_secret_access_key = os.environ[secret_key]   )
+        self._s3 = self.session.resource(service_name = 's3', endpoint_url = endpoint_url) #by using underscore beforevariable we are making the variable is protected 
+        self._bucket = self._s3.Bucket(bucket)
+
+    def list_files_in_prefix(self):
+        pass
+
+    def read_csv_to_df(self):
+        pass
+
+    def write_df_to_s3(self):
+        pass
