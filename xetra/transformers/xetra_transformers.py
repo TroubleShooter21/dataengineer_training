22b35b4290ce6c3f524e423a7c403b35be4beb94
FedeC21 <94315894+FedeC21@users.noreply.github.com>
2024-03-22 16:51:41 +0100

    updated xetyra_tranformerfile

--- xetra/transformers/xetra_transformers.py
+++ xetra/transformers/xetra_transformers.py
@@ -26,3 +26,48 @@ class XetraSourceConfig(NamedTuple):
     src_col_min_price:str
     src_col_max_price:str
     src_col_traded_vol:str
+
+
+class XetraTargetConfig(NamedTuple):
+    '''
+    Class for Target  configuration 
+
+
+    '''
+    trg_col_isin: str
+    trg_col_date: str
+    trg_col_op_price: str
+    trg_col_clos_price: str
+    trg_col_min_price: str
+    trg_col_max_price: str
+    trg_col_dail_trad_vol:str
+    trg_col_ch_prev_clos:str
+    trg_key:str
+    trg_key_date_format:str
+    trg_format:str
+
+
+class XetraETL():
+    '''
+    reads the Xetra data, transforms and writes tranformed to target '''
+    def __init__(self, s3_bucket_src: S3BucketConnector, s3_bucket_trg:S3BucketConnector, meta_key:str ,src_args: XetraSourceConfig, trg_args: XetraTargetConfig) -> None:
+        self.s3_bucket_src = s3_bucket_src
+        self.s3_bucket_trg= s3_bucket_trg
+        self.meta_key = meta_key
+        self.src_args = src_args
+        self.trg_args = trg_args
+        self.extract_date = 
+        self.extract_date_list = 
+        self.meta_update_list = 
+
+    def extract(self):
+        pass
+
+    def tranform_report1(self):
+        pass
+    
+    def load(self):
+        pass
+
+    def etl_report1(self):
+        pass
